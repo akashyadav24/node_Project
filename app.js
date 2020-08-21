91135dabe60b16b384e419ada4789548d444337f
akash yadav <akashyadavmrt24@gmail.com>
2020-08-21 00:39:19 -0700

    Express

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
+const app = express()
+
+mongoose.connect(url,{u})
---
+++ models/alien.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const alienSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+        tech: {
+            type: String,
+            require:true
+         },
+         sub:{
+             type: Boolean,
+             require: true,
+             default:false
+         }
+    
+    })
+module.exports = mongoose.model('aliensSchema')
